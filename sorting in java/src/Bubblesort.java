92d56cb23fb1460d8794bf27dc5951faa299f045
agrawalkeshav10 <keshavagrawal1043@gmail.com>
2026-01-12 18:20:45 +0530

    bubble sorting

---
+++ sorting in java/src/Bubblesort.java
@@ -0,0 +1,40 @@
+
+public class Bubblesort {
+	
+		
+	public static void printarray(int arr[]) {
+		for(int i =0;i<arr.length;i++) {
+			
+			System.out.print(arr[i]+" ");
+		}
+		
+		System.out.println();
+		
+	}
+	
+public static void main(String args[]) {
+	
+	// bubble sort;
+	
+	int arr[]= {7,8,3,2,1};
+	
+	for(int i=0;i<arr.length-1;i++) {
+		
+		for(int j=0;j<arr.length-i-1;j++) {
+			
+			if(arr[j]> arr[j+1]) {
+				
+				// swap 
+
+				int temp = arr[j];
+				
+				arr[j]=arr[j+1];
+				
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printarray(arr);
+}
+
+}
